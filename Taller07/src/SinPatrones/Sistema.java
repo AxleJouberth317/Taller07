264647f8a3f05424fa45d1bf55af50d74312e484
Administrador <Administrador@GGIECUT1WRK025.espol.edu.ec>
2018-07-19 12:57:49 -0500

    hi

--- Taller07/src/SinPatrones/Sistema.java
+++ Taller07/src/SinPatrones/Sistema.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package SinPatrones;
 
-/**
- *
- * @author Administrador
- */
+import java.util.Scanner;
+
 public class Sistema {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+	public static void main(String[] args) {
+//		El mismo sistema se lo va vender a 2 locales de comida.
+		Scanner scanner = new Scanner(System.in);
+		
+		String menu = "  *** Sistema para preparar comida automáticamente ***\n"
+				+ "1. Esquina de Ales\n"
+				+ "2. Pollos Gus\n"
+				+ "3. Salir del sistema\n"
+				+ "Seleccione una opción:";
+		System.out.println(menu);
+		String opcion = scanner.nextLine();
+		while (! opcion.equals("3")){
+			if (opcion.equals("1")){
+				System.out.println("Bienvenido a la Esquina de Ales");
+//				Preparar un Arroz con menestra y pollo
+			}else if(opcion.equals("2")){
+				System.out.println("Bienvenido a Pollos Gus");
+//				Preparar un Arroz con menestra y pollo
+			}else{
+				System.out.println("Opción incorrecta");
+			}
+			System.out.println(menu);
+			opcion = scanner.nextLine();
+			
+		}
+		System.out.println("Se cerró el programa");
+	}
+	
 }
